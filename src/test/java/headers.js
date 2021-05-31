44f7de9feabf8fe1a7f1b912271de4300eb6d12c
Sarthak <47483105+sarthakmay31@users.noreply.github.com>
2021-05-31 13:05:45 +0530

    Update headers.js

--- src/test/java/headers.js
+++ src/test/java/headers.js
@@ -1,5 +1,5 @@
 headers = {
-			'x-rapidapi-key': "79b4e3e020msh96182e0b1ca3bdcp1c86edjsnd6897ac5ed53",
+			'x-rapidapi-key': "NotAdded",
 			'x-rapidapi-host': "body-mass-index-bmi-calculator.p.rapidapi.com"
 		
  }
